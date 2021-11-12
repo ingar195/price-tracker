98ab8d48b5b962809977a3dc7f9d97bd59eff9d6
ingar195 <ingar@megarden.no>
2021-11-12 17:42:57 +0100

    Added write to json

--- tracker.py
+++ tracker.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from pushbullet import Pushbullet
 import json
 
+
 def get(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -33,39 +34,59 @@ def komplett(soup, url):
     return name, price, stock
 
 
-def Notify(Name, CurrentState):
+def Notify(alert):
     apiKey = ""
     with open("pushbullet_api_key.txt", "r") as f:
         apiKey = f.readline()
     pb = Pushbullet(apiKey)
-    pb.push_note(Name, CurrentState)
-
+    if len(alert) != 1:
+        cnt = 0
+        for al in alert:
+            if cnt != 0:
+                print(f"Allert {al}")
+                pb.push_note(alert[0], al)
+            cnt += 1
 
 def site(url, data):    
     if "komplett" in url:
-        writeConfig(komplett(get(url), url), data)
+        writeConfig(komplett(get(url), url), data, url)
     else:
         print(f"Not supported url {url}")
     
 
-def writeConfig(returnFromStore, data):
-    name = returnFromStore[0]
-    stock = returnFromStore[1]
-    price = returnFromStore[2]
-    print(f"name: {name}, stock: {stock}, price: {price}")
+def writeConfig(returnFromStore, data, url):
     
+    name = returnFromStore[0]
+    price = returnFromStore[1]
+    stock = returnFromStore[2]
+    print(f"name: {name}, stock: {stock}, price: {price}")
+    alert = [name]
+    if data[url]["Name"] == "":
+        data[url]["Name"] = name
+        data[url]["Price"] = price
+        data[url]["Stock"] = stock
+    else:
+        if data[url]["Price"] != price:
+            alert.append("Price changed from {} to {}".format(data[url]["Price"], price))
+            data[url]["Price"] = price 
+
+        if data[url]["Stock"] != stock:
+            alert.append("Stock changed from {} to {}".format(data[url]["Stock"], stock))
+            data[url]["Stock"] = stock
+    
+    with open(jsonFile, "w+") as f:
+            f.write(json.dumps(data))
+
+    Notify(alert)
 
 def readConfig():
-    with open("products.json") as jsonFile:
-        return json.load(jsonFile)
+    with open(jsonFile, "r") as jf:
+        return json.load(jf)
 
 
-
-
-
-
+jsonFile = "products.json"
 data = readConfig()
 
 for url in data:
     print(url)
-    site(url, data[url])
+    site(url, data)
